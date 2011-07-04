2690ae588ddab3c147d7076670568d36a8b51e20
Masato Bito <masato@bz2.jp>
2011-07-04 14:31:46 +0900

    added template class

---
+++ src/Shogi/Kifu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Shogi_Kifu
+ * @author Masato Bito <masato@bz2.jp>
+ * License: MIT License
+ * @package Shogi_Kifu
+ */
+
+class Shogi_Kifu
+{
+}
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2: */
