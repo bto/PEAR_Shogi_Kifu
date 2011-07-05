36e43515818e1bd46207923f1c323a5b683a0b48
Masato Bito <masato@bz2.jp>
2011-07-05 18:40:28 +0900

    updated sample

--- sample/sample.php
+++ sample/sample.php
@@ -1,5 +1,7 @@
 <?php
 require_once(dirname(dirname(__FILE__)).'/src/Shogi/Kifu.php');
 
+mb_internal_encoding('UTF-8');
+
 $data = file_get_contents(dirname(__FILE__).'/081217-h.kif');
 $kifu = new Shogi_Kifu($data, 'kif');
