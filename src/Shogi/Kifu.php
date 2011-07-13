3f1b9b846c309c0e953956559e00030e32fd1d32
Masato Bito <masato@bz2.jp>
2011-07-14 02:48:32 +0900

    implemented Shogi_Kifu->prepare()

--- src/Shogi/Kifu.php
+++ src/Shogi/Kifu.php
@@ -10,6 +10,60 @@ require_once(dirname(__FILE__).'/Kifu/Suite.php');
 
 class Shogi_Kifu
 {
+  protected $number_x_map = array(
+    1 => '１',
+    2 => '２',
+    3 => '３',
+    4 => '４',
+    5 => '５',
+    6 => '６',
+    7 => '７',
+    8 => '８',
+    9 => '９');
+
+  protected $number_y_map = array(
+    1 => '一',
+    2 => '二',
+    3 => '三',
+    4 => '四',
+    5 => '五',
+    6 => '六',
+    7 => '七',
+    8 => '八',
+    9 => '九');
+
+  protected $piece_map = array(
+    'FU' => 'FU',
+    'KY' => 'KY',
+    'KE' => 'KE',
+    'GI' => 'GI',
+    'KI' => 'KI',
+    'KA' => 'KA',
+    'HI' => 'HI',
+    'OU' => 'OU',
+    'TO' => 'FU',
+    'NY' => 'KY',
+    'NK' => 'KE',
+    'NG' => 'GI',
+    'UM' => 'KA',
+    'RY' => 'HI');
+
+  protected $piece_string_map = array(
+    'FU' => '歩',
+    'KY' => '香',
+    'KE' => '桂',
+    'GI' => '銀',
+    'KI' => '金',
+    'KA' => '角',
+    'HI' => '飛',
+    'OU' => '王',
+    'TO' => 'と',
+    'NY' => '成香',
+    'NK' => '成桂',
+    'NG' => '成銀',
+    'UM' => '馬',
+    'RY' => '竜');
+
   public function __construct($source = null, $format = null)
   {
     $this->suite_init = new Shogi_Kifu_Suite();
@@ -39,7 +93,7 @@ class Shogi_Kifu
     $this->parser->parse();
     $this->prepare();
 
-    $this->black = $this->info['player_start'] == 'black';
+    $this->black = $this->info['player_start'] === 'black';
     $this->step  = 0;
     $this->suite = clone $this->suite_init;
 
@@ -48,19 +102,66 @@ class Shogi_Kifu
 
   public function prepare()
   {
-    $info       =& $this->info;
-    $moves      =& $this->moves;
-    $suite_init =& $this->suite_init;
+    $info =& $this->info;
 
     if (!isset($info['player_start'])) {
-      if (isset($info['handicap']) && $info['handicap'] !== 'Even') {
-        $info['player_start'] = 'white';
-      } else {
-        $info['player_start'] = 'black';
-      }
+      $info['player_start'] = 'black';
     }
 
-    foreach ($this->moves->records as $i => &$move) {
+    $suite        = clone $this->suite_init;
+    $move_records = $this->moves->records;
+    foreach ($move_records as $i => &$move) {
+      if ($move['type'] !== 'move') {
+        continue;
+      }
+      print_r($move);
+
+      $move_prev =  $move_records[$i-1];
+      $from      =& $move['from'];
+      $to        =& $move['to'];
+
+      if (!isset($move['is_black'])) {
+        $move['is_black'] = !$move_prev['is_black'];
+      }
+
+      if (!isset($from['piece'])) {
+        if ($from['x']) {
+          $from['piece'] = $suite->board[$from['x']][$from['y']]['piece'];
+        } else {
+          $from['piece'] = $to['piece'];
+        }
+      }
+
+      if (!$to['x']) {
+        $to['x'] = $move_prev['to']['x'];
+        $to['y'] = $move_prev['to']['y'];
+      }
+
+      if (!isset($move['stand'])) {
+        $move['stand'] = null;
+      }
+
+      if ($cell = $suite->board[$to['x']][$to['y']]) {
+        $move['stand'] = array(
+          'piece' => $cell['piece'],
+          'stand' => $this->piece_map[$cell['piece']]);
+      }
+
+      if (!isset($move['str'])) {
+        $str  = $this->number_x_map[$to['x']];
+        $str .= $this->number_y_map[$to['y']];
+        $str .= $this->piece_string_map[$from['piece']];
+        if ($from['piece'] !== $to['piece']) {
+          $str .= '成';
+        }
+        if (!$from['x']) {
+          $str .= '打';
+        }
+        $move['str'] = $str;
+      }
+
+      $suite->move($move);
+      print_r($move);
     }
   }
 
